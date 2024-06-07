b05d9d8f64092767d92df2ebf117511162e7d8af
Neur0nz <72764841+Neur0nz@users.noreply.github.com>
2024-06-07 18:43:52 +0300

    Quartz sync: Jun 7, 2024, 6:43 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,14 +8,14 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Nadavi's Blog",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "neur0nz.github.io/NP-Blog/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
