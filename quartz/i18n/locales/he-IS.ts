60ec72c49e775bae632aac52e80b98088fe2a8b9
Neur0nz <nadavprr@gmail.com>
2025-02-17 23:29:42 +0200

    Update he-IS.ts

--- quartz/i18n/locales/he-IS.ts
+++ quartz/i18n/locales/he-IS.ts
@@ -30,7 +30,7 @@ export default {
       darkMode: "Dark mode",
     },
     explorer: {
-      title: "מצא פוסטים",
+      title: "פוסטים",
     },
     footer: {
       createdWith: "נוצר באמצעות",
